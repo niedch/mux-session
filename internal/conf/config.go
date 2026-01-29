a700a2c2432637776c043471d6a5e9ef151765ec
nic <christoph.niederer99@gmail.com>
2026-01-29 12:29:18 +0100

    refactor(validation): Move Validation

--- internal/conf/config.go
+++ internal/conf/config.go
@@ -1,7 +1,6 @@
 package conf
 
 import (
-	"errors"
 	"log"
 
 	"github.com/knadh/koanf/parsers/toml"
@@ -59,57 +58,6 @@ func loadProjectConfig(k *koanf.Koanf) error {
 		log.Fatal("Cannot load config from 'config.toml'", err)
 		return err
 	}
-	return nil
-}
-
-func validateConfig(conf *Config) error {
-	for _, project := range conf.Project {
-		if err := validateProjectConfig(project); err != nil {
-			return err
-		}
-	}
-
-	return validateProjectConfig(conf.Default)
-}
-
-func validateProjectConfig(project ProjectConfig) error {
-	if err := validatePrimaryMarker(project); err != nil {
-		return err
-	}
-
-	if err := validatePanelConfig(project); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func validatePrimaryMarker(project ProjectConfig) error {
-	primaryCount := 0
-	for _, window := range project.WindowConfig {
-		// Check if this window is marked as primary
-		if window.Primary != nil && *window.Primary {
-			primaryCount++
-		}
-
-		// Validate that only one window is marked as primary
-		if primaryCount > 1 {
-			return errors.New("only one window can be marked as primary in project configuration")
-		}
-	}
-
-	return nil
-}
-
-func validatePanelConfig(project ProjectConfig) error {
-	for _, window := range project.WindowConfig {
-		for _, panel := range window.PanelConfig {
-			if panel.PanelDirection != "v" && panel.PanelDirection != "h" {
-				return errors.New("panel_direction must be 'v' or 'h'")
-			}
-		}
-	}
-
 	return nil
 }
 
