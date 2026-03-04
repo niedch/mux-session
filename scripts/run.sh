b8243e98fa01d54560fa4876a09fe6a9b0d77d60
nic <christoph.niederer99@gmail.com>
2026-03-04 07:35:47 +0100

    fix: Script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-$MUX_INSTALL_DIR="/usr/local/bin"
+MUX_INSTALL_DIR=/usr/local/bin
 
-if command -v $MUX_INSTALL_DIR/mux-session >/dev/null 2>&1; then
-    $MUX_INSTALL_DIR/mux-session -v
+if command -v "$MUX_INSTALL_DIR/mux-session" >/dev/null 2>&1; then
+    $MUX_INSTALL_DIR/mux-session
 else
     echo "mux-session not found, installing..."
     SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
     "$SCRIPT_DIR/../install.sh"
     $MUX_INSTALL_DIR/mux-session
 fi
-set +e
+
 
